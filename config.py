b813314d3ea69ca1e8aa6f1a3ba202788ce84b76
Viktoria Artemiak <toriartemiak@gmail.com>
2025-10-09 01:38:01 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,8 @@
+import os
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:17102416@127.0.0.1:3306/CarSalesDBLAB3'
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL",
+        "mysql+pymysql://admin:password@127.0.0.1:3306/CarSalesDBLAB3"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
